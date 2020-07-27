54b0541c4a0ba1914916f6ce4387feaf3cefea89
wa545464 <1982414933@qq.com>
2020-07-27 16:18:01 +0800

    项目基本目录结构设置

--- src/pages/alipay.vue
+++ src/pages/alipay.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <div></div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  data () {
+    return {
+
+    }
+  },
   components: {
+
+  },
+  methods: {
+
   }
 }
 </script>
 
-<style>
+<style scoped>
 </style>
